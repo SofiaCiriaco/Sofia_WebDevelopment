ce93c4c68499fd4ee019ec59a996ebee7302f0c3
SofiaCiriaco <2025q0037@pwu.edu.ph>
2026-09-23 16:46:58 +0800

    finals

--- FinalActivity.js
+++ FinalActivity.js
@@ -3,8 +3,30 @@ function add(){
    let num1 = Number(document.getElementById("num1").value);
    let num2 = Number(document.getElementById("num2").value);
    document.getElementById("result").innerHTML = num1 + num2;
+}
+function Subtract(){
+   let num1 = Number(document.getElementById("num1").value);
+   let num2 = Number(document.getElementById("num2").value);
    document.getElementById("result").innerHTML = num1 - num2;
    document.getElementById("result").innerHTML = num1 * num2;
    document.getElementById("result").innerHTML = num1 / num2;
    document.getElementById("result").innerHTML = num1 % num2;
+}
+function Multiply(){
+   let num1 = Number(document.getElementById("num1").value);
+   let num2 = Number(document.getElementById("num2").value);
+   document.getElementById("result").innerHTML = num1 * num2;
+}
+function Divide(){
+   let num1 = Number(document.getElementById("num1").value);
+   let num2 = Number(document.getElementById("num2").value);
+   document.getElementById("result").innerHTML = num1 / num2;
+}
+function Remainder(){
+   let num1 = Number(document.getElementById("num1").value);
+   let num2 = Number(document.getElementById("num2").value);
+   document.getElementById("result").innerHTML = num1 % num2;
+}
+function clearResult() {
+    document.getElementById("result").innerHTML = "";
 }
