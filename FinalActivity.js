f5fb3b947144d6cb535f3668fc44d0cf6e07c343
SofiaCiriaco <2025q0037@pwu.edu.ph>
2026-09-23 14:43:58 +0800

    Finals Web Dev

--- FinalActivity.js
+++ FinalActivity.js
@@ -1,5 +1,10 @@
+
 function add(){
    let num1 = Number(document.getElementById("num1").value);
    let num2 = Number(document.getElementById("num2").value);
    document.getElementById("result").innerHTML = num1 + num2;
+   document.getElementById("result").innerHTML = num1 - num2;
+   document.getElementById("result").innerHTML = num1 * num2;
+   document.getElementById("result").innerHTML = num1 / num2;
+   document.getElementById("result").innerHTML = num1 % num2;
 }
